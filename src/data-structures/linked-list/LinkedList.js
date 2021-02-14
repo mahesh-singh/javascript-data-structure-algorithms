3a8431dd74d8efde6b8cfa78e524d8ff6cbe11af
mahesh singh <singh.mahesh@gmail.com>
2021-02-14 19:08:12 +0530

    added linked list

--- src/data-structures/linked-list/LinkedList.js
+++ src/data-structures/linked-list/LinkedList.js
@@ -44,6 +44,70 @@ export default class LinkedList{
         return null;
     }
 
+    delete(value){
+        if(!this.head){
+            return null;
+        }
+
+        let deletedNode = null;
+        
+        //first check for head node
+        if(this.head.value === value){
+            deletedNode = this.head;
+            this.head = this.head.next;
+            return deletedNode;
+        }
+
+        //Check for rest of the node, except tail
+        let currentNode = this.head;
+        while(currentNode.next){
+            if(currentNode.next.value === value){
+                deletedNode = currentNode.next;
+                currentNode.next = currentNode.next.next;
+                return deletedNode;
+            }
+            currentNode = currentNode.next;
+        }
+        //check for the tail
+        if(this.tail.value === value){
+            deletedNode = this.tail;
+            this.tail = currentNode;
+            return deletedNode;
+        }
+
+        return deletedNode;
+    }
+
+    reverse(){
+        // We can do this by reversing the links by visiting one node at a time.
+
+        let currentNode  = this.head;
+        let nextNode =  null;
+        let previousNode  = null;
+
+        while(currentNode){
+            //1. store the next node, otherwise will loose the next node
+            nextNode = currentNode.next;
+            
+            //2. set the current node's next node to previous node
+            currentNode.next = previousNode;
+
+            //3. set the previous node to current node for next iteration
+            previousNode = currentNode;
+
+            //4. move to next node by setting current to next node, which store in first step
+            currentNode = nextNode;
+        }
+
+        //set the tail which is current head
+        this.tail = this.head;
+
+        //set the head, previousNode (in last iteration cycle it will set to tail) will become new head
+        this.head = previousNode;
+        return this;
+
+    }
+
     toArray(){
         if(!this.head){
             return [];
--- src/data-structures/linked-list/__test__/LinkedList.test.js
+++ src/data-structures/linked-list/__test__/LinkedList.test.js
@@ -81,12 +81,64 @@ describe("LinkedList find method", ()=>{
 });
 
 describe("LinkedList toArray method", ()=>{
-    it("should return deleted node if value exist in linked list", ()=>{
+    it("should return array of values for toArray method", ()=>{
         const linkedList = new LinkedList();
         linkedList.append(1).append(2).append(3).append(4);
         const result = linkedList.toArray();
         expect(JSON.stringify(result)).toBe(JSON.stringify([1,2,3,4]));
-        expect(result).toBe([1,2,3,4]);
+        expect(result).toEqual([1,2,3,4]);
+    });
+    it("should return empty array if linked list doesn't have any node", ()=> {
+        const linkedList = new LinkedList();
+        const result = linkedList.toArray();
+        expect([]).toEqual([]);
+    });
+});
+
+describe("LinkedList delete method", ()=> {
+    it("should return deleted node if value exist in the linked list", ()=>{
+        const linkedList = new LinkedList();
+        linkedList.append(1).append(2).append(3).append(4);
+        const result = linkedList.delete(3);
+        expect(result.value).toBe(3);
+        expect(linkedList.toArray()).toEqual([1,2,4]);
+    });
+
+    it("should return null if value doesn't exist in linked list", ()=>{
+        const linkedList = new LinkedList();
+        linkedList.append(1).append(2).append(3).append(4);
+        const result = linkedList.delete(6);
+        expect(result).toBe(null);
+        expect(linkedList.toArray()).toEqual([1,2,3,4]);
+    });
+
+    it("should return node if values exist on head of the linked list", ()=>{
+        const linkedList = new LinkedList();
+        linkedList.append(1).append(2).append(3).append(4);
+        const result = linkedList.delete(1);
+        expect(result.value).toBe(1);
+        expect(linkedList.toArray()).toEqual([2,3,4]);
+    });
+
+    it("should return the node if value exist on tail of the linked", ()=> {
+        const linkedList = new LinkedList();
+        linkedList.append(1).append(2).append(3).append(4);
+        const result = linkedList.delete(4);
+        expect(result.value).toBe(4);
+        expect(linkedList.toArray()).toEqual([1,2,3]);
+    });
+});
+
+describe("LinkedList reverse method", ()=>{
+    it("should return the reverse the linked list for non empty linked list", ()=> {
+        const linkedList = new LinkedList();
+        linkedList.append(1).append(2).append(3).append(4);
+        const result = linkedList.reverse();
+        expect(result.toArray()).toEqual([4, 3,2,1]);
+    });
+    it("should return empty linkedList for empty linkedList", ()=>{
+        const linkedList = new LinkedList();
+        expect(linkedList.toArray()).toEqual([]);
     });
 });
 
